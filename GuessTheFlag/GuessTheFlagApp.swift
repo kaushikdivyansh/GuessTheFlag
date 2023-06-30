9741bcebf29c6436d24b27b78303b84d3c6157c2
Divyansh Kaushik <kaushik.divyansh@icloud.com>
2023-06-30 11:21:45 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Divyansh Kaushik on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
